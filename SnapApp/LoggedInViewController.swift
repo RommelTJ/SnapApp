fc381de1c1f8f57a5bb1f955584e15d7ba203972
Rommel Rico <rommeltj@gmail.com>
2015-04-25 20:24:20 -0700

    Storing phrases

--- SnapApp/LoggedInViewController.swift
+++ SnapApp/LoggedInViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class LoggedInViewController: UIViewController {
 
+    var phrases = PFObject(className: "phrases")
     @IBOutlet weak var phrase1: UITextField!
     @IBOutlet weak var phrase2: UITextField!
     @IBOutlet weak var phrase3: UITextField!
@@ -17,6 +18,40 @@ class LoggedInViewController: UIViewController {
     @IBOutlet weak var successLabel: UILabel!
     
     @IBAction func doUpdate(sender: AnyObject) {
+        phrases["phrase1"] = phrase1.text
+        phrases["phrase2"] = phrase2.text
+        phrases["phrase3"] = phrase3.text
+        phrases["phrase4"] = phrase4.text
+        
+        var query = PFQuery(className: "phrases")
+        var username = PFUser.currentUser()?.username
+        query.whereKey("username", equalTo: username!)
+        query.findObjectsInBackgroundWithBlock { (objects, error) -> Void in
+            if error == nil && objects!.count > 0 {
+                if let storedPhrases = objects![0] as? PFObject {
+                    storedPhrases["phrase1"] = self.phrase1.text
+                    storedPhrases["phrase2"] = self.phrase2.text
+                    storedPhrases["phrase3"] = self.phrase3.text
+                    storedPhrases["phrase4"] = self.phrase4.text
+                    
+                    storedPhrases.saveInBackgroundWithBlock({ (success, error) -> Void in
+                        if error == nil {
+                            self.successLabel.text = "Phrases updated"
+                        } else {
+                            self.successLabel.text = "There was an error"
+                        }
+                    })
+                    
+                } else {
+                    self.successLabel.text = "Please try again"
+                }
+            } else {
+                let errorString = error!.userInfo?["error"] as? String
+                self.successLabel.text = errorString
+                NSLog("Error in Updating Phrases")
+            }
+        }
+        phrases.pin()
     }
     
     @IBAction func doLogout(sender: AnyObject) {
@@ -28,7 +63,31 @@ class LoggedInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let query = PFQuery(className: "phrases")
+        query.fromLocalDatastore()
+        var username = PFUser.currentUser()?.username
+        query.whereKey("username", equalTo: username!)
+        
+        query.findObjectsInBackgroundWithBlock { (objects, error) -> Void in
+            if error == nil && objects!.count > 0 {
+                if let object = objects![0] as? PFObject {
+                    self.phrases = object
+                }
+            } else {
+                self.phrases["username"] = PFUser.currentUser()?.username
+                self.phrases["phrase1"] = "I'll be late!"
+                self.phrases["phrase2"] = "You are awesome"
+                self.phrases["phrase3"] = "I'm hungry"
+                self.phrases["phrase4"] = "Talk to you later"
+                self.phrases.save()
+                self.phrases.pin()
+            }
+            
+            self.phrase1.text = self.phrases["phrase1"] as! String
+            self.phrase2.text = self.phrases["phrase2"] as! String
+            self.phrase3.text = self.phrases["phrase3"] as! String
+            self.phrase4.text = self.phrases["phrase4"] as! String
+        }
     }
 
     override func didReceiveMemoryWarning() {
